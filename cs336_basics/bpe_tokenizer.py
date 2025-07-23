598416c305bcb433422760b9782647d76acdad9a
Filippo Buoncompagni <filippo.buonco95@gmail.com>
2025-07-23 20:03:24 +0200

    make the methods private

--- cs336_basics/bpe_tokenizer.py
+++ cs336_basics/bpe_tokenizer.py
@@ -10,8 +10,9 @@ class BPETokenizer():
         self.vocab: dict[int, bytes] = {}
         self.token_merges: list[tuple[bytes, bytes]] = []
         self.next_token_id = 256 + len(self.special_tokens)
+        self._initialize_vocabulary()
     
-    def initialize_vocabulary(self) -> None:
+    def _initialize_vocabulary(self) -> None:
         # Base vocab
         for i in range(256):
             self.vocab[i] = bytes([i])
@@ -21,40 +22,39 @@ class BPETokenizer():
             self.vocab[i] = token.encode("utf-8")
     
     def train(self, input_path: str | os.PathLike):
-        self.initialize_vocabulary()
         pretoken_counts = get_pretoken_counts(input_path, self.special_tokens)
         byte_pretoken_counts = convert_string_to_bytes(pretoken_counts)
         while self.next_token_id < self.vocab_size:
-            pair_counts = self.get_pair_counts(byte_pretoken_counts)
-            most_frequent_pair = self.get_most_frequent_pair(pair_counts)
-            self.create_new_token_and_update_vocab_and_merges(most_frequent_pair)
-            byte_pretoken_counts = self.update_byte_pretoken_counts(byte_pretoken_counts, most_frequent_pair)
+            pair_counts = self._get_pair_counts(byte_pretoken_counts)
+            most_frequent_pair = self._get_most_frequent_pair(pair_counts)
+            self._create_new_token_and_update_vocab_and_merges(most_frequent_pair)
+            byte_pretoken_counts = self._update_byte_pretoken_counts(byte_pretoken_counts, most_frequent_pair)
             self.next_token_id += 1
         
         return self.vocab, self.token_merges
 
 
-    def get_pair_counts(self, byte_pretoken_counts: Counter) -> Counter:
+    def _get_pair_counts(self, byte_pretoken_counts: Counter) -> Counter:
         pair_counts: Counter = Counter()
         for seq, freq in byte_pretoken_counts.items():
             for i in range(len(seq) - 1):
                 pair_counts[(seq[i], seq[i + 1])] += freq
         return pair_counts
     
-    def get_most_frequent_pair(self, pair_counts: Counter) -> tuple[int, int]:
+    def _get_most_frequent_pair(self, pair_counts: Counter) -> tuple[int, int]:
         most_frequent_pair, _ = max(
             pair_counts.items(),
             key=lambda x: (x[1],) + tuple(self.vocab[c] for c in x[0])
         )
         return most_frequent_pair
     
-    def create_new_token_and_update_vocab_and_merges(self, most_frequent_pair: tuple[int, int]) -> None:
+    def _create_new_token_and_update_vocab_and_merges(self, most_frequent_pair: tuple[int, int]) -> None:
         id1, id2 = most_frequent_pair
         new_token_bytes = self.vocab[id1] + self.vocab[id2]
         self.vocab[self.next_token_id] = new_token_bytes
         self.token_merges.append((self.vocab[id1], self.vocab[id2]))
     
-    def update_byte_pretoken_counts(self, byte_pretoken_counts: Counter, most_frequent_pair: tuple[int, int]) -> Counter:
+    def _update_byte_pretoken_counts(self, byte_pretoken_counts: Counter, most_frequent_pair: tuple[int, int]) -> Counter:
         new_counts: Counter = Counter()
         id1, id2 = most_frequent_pair
         for seq, freq in byte_pretoken_counts.items():
